fd64571cd4f5dd7379d2d589035683652a879ef4
ani <godwin@istrategytech.com>
2023-06-05 15:28:46 +0100

    music player ui:wip

--- app/src/main/java/com/example/anismusicplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/anismusicplayerapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.anismusicplayerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val songTitle  = findViewById<TextView>(R.id.song_title)
+        val rewind = findViewById<Button>(R.id.rewind)
+        val fastForward = findViewById<Button>(R.id.fast_forward)
+        val play = findViewById<Button>(R.id.play)
+        val pause = findViewById<Button>(R.id.pause)
+        val seekBar = findViewById<SeekBar>(R.id.seek_bar)
     }
 }
